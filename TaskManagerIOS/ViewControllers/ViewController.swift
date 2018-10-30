0ac306f4bf7716d7f857d8c98415d8cd6ac65882
Spencer Casteel <spencercasteel@gmail.com>
2018-10-30 11:00:35 -0500

    added stuff

--- TaskManagerIOS/ViewControllers/ViewController.swift
+++ TaskManagerIOS/ViewControllers/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
