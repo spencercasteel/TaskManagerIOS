12b5206f0a3489d0547d77b89d16ebaa1b6361c3
Spencer Casteel <spencercasteel@gmail.com>
2018-10-30 10:05:48 -0500

    initial commit

---
+++ TaskManagerIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagerIOS
+//
+//  Created by Spencer Casteel on 10/30/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
